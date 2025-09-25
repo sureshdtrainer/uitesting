1f8af2242c93cd739a46993e52709cddca4e1ab3
sureshdtrainer <suresh.d.trainer@gmail.com>
2025-09-25 14:25:13 +0530

    refactored to add fixture

--- tests/test_automation.py
+++ tests/test_automation.py
@@ -4,19 +4,27 @@ from playwright.sync_api import sync_playwright
 # first test case to verify google title
 
 
-def test_google_title(playwright):
-    browser = playwright.chromium.launch(headless=False)
+@pytest.fixture(scope="module")
+def browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        yield browser
+        browser.close()
+
+
+@pytest.fixture(scope="function")
+def page(browser):
     page = browser.new_page()
+    yield page
+    page.wait_for_timeout(3000)
+    page.close()
+
+
+def test_google_title(page):
     page.goto("https://www.google.com")
     assert page.title() == "Google"
-    page.wait_for_timeout(3000)
-    browser.close()
 
 
-def test_orangehrm_title(playwright):
-    browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+def test_orangehrm_title(page):
     page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     assert page.title() == "OrangeHRM"
-    page.wait_for_timeout(3000)
-    browser.close()
