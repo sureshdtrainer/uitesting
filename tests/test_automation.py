462c8df03e24bd69defa6a28dda5c42f6e199d3c
sureshdtrainer <suresh.d.trainer@gmail.com>
2025-09-25 15:21:33 +0530

    using playwright expect instead of assert and added gitignore

--- tests/test_automation.py
+++ tests/test_automation.py
@@ -1,5 +1,5 @@
 import pytest
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
 
 # first test case to verify google title
 
@@ -22,12 +22,14 @@ def page(browser):
 
 def test_google_title(page):
     page.goto("https://www.google.com")
-    assert page.title() == "Google"
+    # assert page.title() == "Google"
+    expect(page).to_have_title("Google")
 
 
 def test_orangehrm_title(page):
     page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    assert page.title() == "OrangeHRM"
+    # assert page.title() == "OrangeHRM"
+    expect(page).to_have_title("OrangeHRM")
 
 
 @pytest.mark.parametrize('username,password', [('testuser1', 'testpwd1'), ('testuser2', 'testpwd2')])
@@ -56,4 +58,6 @@ def test_success_orangehrm_login(page, username, password):
         '.oxd-topbar-header-breadcrumb-module')
     dashboard_name = dashboard_breadcrumb.text_content()
     print("Dashboard name:", dashboard_name)
-    assert "Dashboard" == dashboard_name
+    # assert "Dashboard" == dashboard_name
+    expect(page.locator(".oxd-topbar-header-breadcrumb-module")
+           ).to_contain_text("Dashboard")
